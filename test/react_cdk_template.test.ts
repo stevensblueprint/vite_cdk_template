c94384622f9f69dce8214dfe45f79deca573f3f1
miguel-merlin <mmerlin@stevens.edu>
2025-01-22 22:03:30 -0500

    Initial commit

---
+++ test/react_cdk_template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ReactCdkTemplate from '../lib/react_cdk_template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/react_cdk_template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ReactCdkTemplate.ReactCdkTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
